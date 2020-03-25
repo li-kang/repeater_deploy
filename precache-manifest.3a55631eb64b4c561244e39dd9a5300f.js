dfcca2fb0d23e3e82712a17f755402ca4ce7f521
li-kang <likang2008@gmail.com>
2020-03-25 11:15:24 +0000

    deploy

---
+++ precache-manifest.3a55631eb64b4c561244e39dd9a5300f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa5e2dce5fa4d78e8bb199162e7278b",
+    "url": "/books/book_bar.jpg"
+  },
+  {
+    "revision": "212ce4cc537fcf17a21a65ff733d9581",
+    "url": "/books/books.json"
+  },
+  {
+    "revision": "4f45fea7324cd3b4a612ccf469b3d1ff",
+    "url": "/books/新概念英音第三册_037F/[Lesson 1]A puma at large.lrc"
+  },
+  {
+    "revision": "bdbfefa5bcb9aa3be17d1ad9cb3bac68",
+    "url": "/books/新概念英音第三册_037F/[Lesson 1]A puma at large.mp3"
+  },
+  {
+    "revision": "abffb1fa395465eb59decd4ab9306afc",
+    "url": "/books/新概念英音第三册_037F/[Lesson 2]Thirteen equals one.lrc"
+  },
+  {
+    "revision": "03e86427ff5c34875dc2a019f8944aa3",
+    "url": "/books/新概念英音第三册_037F/[Lesson 2]Thirteen equals one.mp3"
+  },
+  {
+    "revision": "eaa5e2dce5fa4d78e8bb199162e7278b",
+    "url": "/books/新概念英音第三册_037F/book.jpg"
+  },
+  {
+    "revision": "2f3e67625753480ca861376cf33cc922",
+    "url": "/books/新概念英音第三册_037F/book.json"
+  },
+  {
+    "revision": "5ae3b5efb85d2a27083b5c2c400bde0c",
+    "url": "/books/新概念英音第三册_037F/开篇语.mp3"
+  },
+  {
+    "revision": "f025228214c996051a55",
+    "url": "/css/app.6dd18649.css"
+  },
+  {
+    "revision": "bfe86a004e2c650bb62d",
+    "url": "/css/chunk-vendors.a6b9ce47.css"
+  },
+  {
+    "revision": "25f175488d6b3c2469972fa8ba0a6843",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f025228214c996051a55",
+    "url": "/js/app.c27587f9.js"
+  },
+  {
+    "revision": "bfe86a004e2c650bb62d",
+    "url": "/js/chunk-vendors.1c576d73.js"
+  },
+  {
+    "revision": "102326dde9a8ca88c1c470764a2ce832",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
